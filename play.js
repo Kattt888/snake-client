300076e7729fe602ad28a3925c17ad9a9a63d917
Katrina <katrinabyrd88@gmail.com>
2024-10-23 10:07:28 -0400

    update code as per ai feedback

--- play.js
+++ play.js
@@ -1,32 +1,10 @@
 const net = require("net");
-const conn = net.createConnection({
-  host: "localhost",
-  port: 3000,
-});
-conn.on("data", (data) => {
-  console.log("Server says: ", data);
-});
-conn.on("connect", () => {
-  conn.write("Hello from client!");
-});
-conn.setEncoding("utf8");
-
-////////
-
-const net = require("net");
-
-const conn = net.createConnection({
-  host: "localhost", // change to IP address of computer, more on that below
-  port: 50541,
-});
-
-conn.setEncoding("utf8"); // interpret data as text
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541 // PORT number here,
+    host: "localhost", // IP address here
+    port: 50541 // PORT number here
   });
 
   // interpret incoming data as text
