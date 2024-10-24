edf6c0f4d03bb472bcc43b247150c94e00a7656e
Katrina <katrinabyrd88@gmail.com>
2024-10-24 00:46:27 -0400

    WASD complete

--- input.js
+++ input.js
@@ -1,16 +1,32 @@
-const setupInput = function () {
-  const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
-  stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
-  stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
-  stdin.resume(); // resume stdin so the program can listen for input
-  stdin.on("data", handleUserInput);
-  return stdin;   // return the stdin object so we can use it elsewhere in the program
-};
+let connection;  // Stores the active TCP connection object
 
-const handleUserInput = function (key) {
-  if (key === "\u0003") {
+const handleUserInput = (key) => {
+  if (key === '\u0003') { // \u0003 maps to ctrl+c input
     process.exit();
+  }
+  // adding movement commands 
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
   }
 };
 
-setupInput();
+const setupInput = (conn) => {
+  connection = conn;  // save the connection object
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,24 +1,16 @@
-const { connect } = require('./client'); // Import the connect function from client.js
-const { setupInput } = require("./input");
+const { connect } = require('./client'); 
+const { setupInput } = require("./input"); 
 
 console.log("Connecting ...");
-connect(); // Use the connect function from client.js
+const connection = connect(); // create connection to the server
+
+// pass the connection object to setupInput
+setupInput(connection); 
 
 // setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
-  stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
-  stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
-  stdin.resume(); // resume stdin so the program can listen for input
-  stdin.on("data", handleUserInput);
-  return stdin;   // return the stdin object so we can use it elsewhere in the program
-};
-
 const handleUserInput = function (key) {
-  if (key === "\u0003") {
-    process.exit();
+  if (key === "\u0003") { // If Ctrl+C is pressed
+    process.exit(); // exit the program
   }
 };
 
-setupInput();
